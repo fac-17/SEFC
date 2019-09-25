5dffd1c03e6a84baa28044bd0e24a3e02bf5ced8
Emmanuel <emanagyei@gmail.com>
2019-09-25 16:13:31 +0100

    initial setup of nameInput file Relates #5

--- src/components/nameInput.js
+++ src/components/nameInput.js
@@ -1,2 +1,16 @@
 // Get an array of input nameS from the player
 // At the beggining of the game
+import React from "react";
+
+const NameInput = () => {
+  const [name, setName] = React.useState("");
+  return (
+    <input
+      type="text"
+      value={name}
+      onChange={event => setName(event.target.value)}
+    />
+  );
+};
+
+export { NameInput };
