707cd21df6bccf90f0dbfebe6166a8c1a9ba2b88
sarah <sarahkharraz@gmail.com>
2019-09-26 13:03:42 +0100

    Create nameOutput function: split the array choosing a random index in the array create a variable with the full array[with a random index attribuate]

--- src/components/nameOutput.js
+++ src/components/nameOutput.js
@@ -1,1 +1,17 @@
-// the random selected name 
+// the random selected name
+import React from "react";
+
+const NameOutput = ({name}) => {
+  console.log(name)
+  // const [person, setPerson] = React.useState("")
+const oneName = name.split(",")
+const randomIndex = Math.floor(Math.random() * oneName.length);
+const randomElement = oneName[randomIndex];
+console.log(randomElement)
+
+  return (
+
+<h1>{randomElement}</h1>
+  )
+}
+export { NameOutput };
