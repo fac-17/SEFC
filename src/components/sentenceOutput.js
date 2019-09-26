ef41e0f24faeee59f08b1c3f30e7c6563d5f2c78
sarah <sarahkharraz@gmail.com>
2019-09-26 18:32:30 +0100

    ISsue with the return filter -> still need to be done

--- src/components/sentenceOutput.js
+++ src/components/sentenceOutput.js
@@ -14,15 +14,15 @@ const SentenceOutput = selection => {
     FetchData(queryUrl, selection).then(data => {
       const dataRecord = data.records;
 
-      const tempCategory = dataRecord.filter(data => {
+      const tempCategory = dataRecord.filter(data => data.fields.Category === selection.selection
         // return data.fields.category === selection;
-        console.log(data.fields.Category);
-        console.log(selection)
-
+        // console.log(data.fields.Category);
+        // console.log(selection.selection)
+        // console.log(data.fields.Category === selection.selection)
       // const filter = tempCategory.filter(cat => cat ? 'truth' : 'dare')
       // console.log(filter)
 
-      });
+      );
     });
   });
 
