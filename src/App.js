90d83b0890037436524720c7c37d90a36e12a739
sarah <sarahkharraz@gmail.com>
2019-09-26 11:09:36 +0100

    Wrap the ternary in divs

--- src/App.js
+++ src/App.js
@@ -9,18 +9,25 @@ function App() {
   return (
     <div className="App">
       <header className="header"></header>
+      {!name ?(
+        <div>
       <section className="nameInput">
         <h2>Welcome to Truth or Dare</h2>
         <p>Enter the players names below</p>
         {<NameInput name = {name}
         setName = {setName}/>}
       </section>
+      </div>
+    ):(
+      <div>
       <section className="checkbox">
         <TruthOrDare />
       </section>
       <section className="sentenceOutput">{/* <SentenceOutput /> */}</section>
       <section className="nameOutput">{/* <NameOutput /> */}</section>
       <section className="nextButton">{/* <NextButton */}</section>
+      </div>
+    )}
     </div>
   );
 }
