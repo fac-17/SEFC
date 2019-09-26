32980b0d726650f0d8594d7a0722e2bd72ae806a
Christine <christine.altmann@gmx.com>
2019-09-26 15:05:26 +0100

    remove private credentials

--- src/components/sentenceOutput.js
+++ src/components/sentenceOutput.js
@@ -7,8 +7,6 @@ const SentenceOutput = () => {
   const airtableToken = process.env.REACT_APP_AIRTABLE_API_KEY;
   const airtableBase = process.env.REACT_APP_AIRTABLE_BASE;
   const queryUrl = `https://api.airtable.com/v0/${airtableBase}/Table%201?api_key=${airtableToken}`;
-  //api.airtable.com/v0/appHuknKWgmu5SFGH/Table%201?api_key=keyKEsyr442uQbgY1
-  https: console.log("im queryUrl", queryUrl);
 
   React.useEffect(() => {
     FetchData(queryUrl).then(data => {
