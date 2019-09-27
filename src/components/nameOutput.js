405901ddf5e426f846c41336b987d95a5ff88566
Christine <christine.altmann@gmx.com>
2019-09-27 14:23:18 +0100

    fix wrongly merged master & branch

--- src/components/nameOutput.js
+++ src/components/nameOutput.js
@@ -18,12 +18,13 @@ const NameOutput = ({ name }) => {
   };
 
   return (
-    <section className="nameOutput">
+    <div className="nameOutput">
       <h1 className="nameOutputh1">{randomElement}</h1>
       <h3 className="nameOutputh3">It's your turn to play!</h3>
-      <button className="outputButton">Next Player</button>
-      </section>
-
+      <button className="outputButton" onClick={handleClick} className="shake">
+        Next Player
+      </button>
+    </div>
   );
 };
 
